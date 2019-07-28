58441eeabf91c4e1f44b3382f453e83472810ff9
Geoff Hutson <geoff.hutson.89@gmail.com>
2019-07-27 22:46:55 -0700

    next commit

--- src/components/Album.js
+++ src/components/Album.js
@@ -17,7 +17,7 @@ class Album extends React.Component {
 
   componentDidMount() {
     const id = this.props.match.params.id;
-    axios.get(`/api/albums/${id}?_embed=photos`).then(resp => {
+    axios.get(`/api/${id}?_embed=photos`).then(resp => {
       this.setState({
         albumName: resp.data.name,
         photos: resp.data.photos
@@ -29,8 +29,10 @@ class Album extends React.Component {
       });
     });
   }
+
   componentWillReceiveProps(newprops) {
     const id = newprops.match.params.id;
+
     axios.get(`/api/albums/${id}?_embed=photos`).then(resp => {
       this.setState({
         albumName: resp.data.name,
@@ -41,7 +43,7 @@ class Album extends React.Component {
 
   render() {
     return (
-      <div>
+      <div id="container">
         <header id="albumHeader">
           <Link to="/">
             <span id="albumArrow">
@@ -50,110 +52,36 @@ class Album extends React.Component {
           </Link>
           <h1>{this.state.albumName}</h1>
         </header>
-        <main>
-          <section>
-            <aside>
-              <ul>
-                {this.state.albums.map(album => (
-                  <Link to={"/album/" + album.id}>
-                    <li>{album.name}</li>
-                  </Link>
-                ))}
-              </ul>
-            </aside>
-          </section>
-          <section className="albumArea">
-            {this.state.photos.map(photo => (
-              <Link to="/indipic">
-                <div className="picture">
-                  <div>
-                    <img src={photo.photo} alt="pic" />
-                  </div>
-                  <div>
-                    <p>{photo.name}</p>
-                  </div>
+
+        <main id="albumArea">
+          {/* <section> */}
+          <aside>
+            <ul>
+              {this.state.albums.map(album => (
+                <Link key={"/album/" + album.id} to={"/ablum/"}>
+                  <li>{album.name}</li>
+                </Link>
+              ))}
+            </ul>
+          </aside>
+          {/* </section> */}
+          {/* <section className="rows"> */}
+          {this.state.photos.map(photo => (
+            <Link to="/photo">
+              <div className="picture">
+                <div>
+                  <img src={photo.photo} alt="pic" />
                 </div>
-              </Link>
-            ))}
-          </section>
+                <div>
+                  <p>{photo.name}</p>
+                </div>
+              </div>
+            </Link>
+          ))}
+          {/* </section> */}
         </main>
       </div>
     );
   }
-  // state = {
-  //   albums: [],
-  //   photos: [],
-  //   albumName: ""
-  // };
-
-  // componentDidMount() {
-  //   const id = this.props.match.params.id;
-  //   axios.get("/api/${id}?_embed=photos").then(resp => {
-  //     this.setState({
-  //       albumName: resp.data.name,
-  //       photos: resp.data.photos
-  //     });
-  //   });
-  //   axios.get("/api/albums").then(resp => {
-  //     this.setState({
-  //       albums: resp.data
-  //     });
-  //   });
-  // }
-
-  // componentWillReceiveProps(newprops) {
-  //   const id = newprops.match.params.id;
-
-  //   axios.get("/api/albums/${id}?_embed=photos").then(resp => {
-  //     this.setState({
-  //       albumName: resp.data.name,
-  //       photos: resp.data.photos
-  //     });
-  //   });
-  // }
-
-  // render() {
-  //   return (
-  //     <div id="container">
-  //       <header id="albumHeader">
-  //         <Link to="/">
-  //           <span id="albumArrow">
-  //             <MaterialIcon icon="arrow_back" />
-  //           </span>
-  //         </Link>
-  //         <h1>{this.state.albumName}</h1>
-  //       </header>
-
-  //       <main id="albumArea">
-  //         <section>
-  //           <aside>
-  //             <ul>
-  //               {this.state.albums.map(album => (
-  //                 <Link to={"/album/" + album.id}>
-  //                   <li>{album.name}</li>
-  //                 </Link>
-  //               ))}
-  //             </ul>
-  //           </aside>
-  //         </section>
-  //         <section className="rows">
-  //           {this.state.photos.map(photo => (
-  //             <Link to="/photo">
-  //               <div className="picture">
-  //                 <div>
-  //                   <img src={photo.photo} alt="pic" />
-  //                 </div>
-  //                 <div>
-  //                   <p>{photo.name}</p>
-  //                 </div>
-  //               </div>
-  //             </Link>
-  //           ))}
-  //         </section>
-  //       </main>
-  //     </div>
-  //   );
-  // }
 }
-
 export default Album;
